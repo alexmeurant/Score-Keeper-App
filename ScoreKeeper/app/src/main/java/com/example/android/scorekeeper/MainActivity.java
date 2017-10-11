1aa4efbf0812ebf02e4689b3c149676afb209e63
MEURANT <alexandremeurant@hotmail.fr>
2017-10-11 20:26:08 +0200

    Added display's method and initialize scores

--- ScoreKeeper/app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ ScoreKeeper/app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.android.scorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreA = 0;
+    int scoreB = 0;
+    private TextView mScoreViewA;
+    private TextView mScoreViewB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        mScoreViewA = (TextView) findViewById(R.id.ScoreA);
+        mScoreViewA.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        mScoreViewB = (TextView) findViewById(R.id.ScoreB);
+        mScoreViewB.setText(String.valueOf(score));
+    }
+
 }
